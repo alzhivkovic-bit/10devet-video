105d897a9c9c453f4a4e4f70279a025e0e35182f
alzhivkovic-bit <al.zhivkovic@gmail.com>
2026-09-19 03:27:24 +0200

    Remove base path from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://10devet.com',
-	base: '/10devet-video',
 	integrations: [sitemap()],
 });
