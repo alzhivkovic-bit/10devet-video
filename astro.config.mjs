1ff603ec4eb8bf878dd8f40d6f7bb8cbb06dd110
alzhivkovic-bit <al.zhivkovic@gmail.com>
2026-09-19 02:44:50 +0200

    Set base path for Astro site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://10devet.com',
+	base: '/10devet-video',
 	integrations: [sitemap()],
 });
